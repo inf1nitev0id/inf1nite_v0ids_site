2c59131f6128d8da287da2c72f21ff1e688a9a9a
inf1nite_v0id <alex-5184@yandex.ru>
2021-01-01 02:57:04 +0300

    Футер теперь подстраивается под текущий год

--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="container-fluid mt-3 py-2 border-top">
 	<div class="row">
 		<div class="col-12 col-md">
-			<small class="text-muted">© 2020</small>
+			<small class="text-muted">© 2020-{{ (new DateTime())->format('Y') }}</small>
 		</div>
 	</div>
 </footer>
